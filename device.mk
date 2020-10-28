799f1dd45a568b6f516fa4e6fb3263a693b73d0c
Woody Lin <woodylin@google.com>
2020-10-28 11:22:19 +0800

    chime: Enable zygote critical window
    Bug: 141948707
    Change-Id: I5dc57312118b0f6d95fc9fe164363ec7e15f1b51
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

--- device.mk
+++ device.mk
@@ -621,3 +621,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+# Zygote critical window.
+PRODUCT_PROPERTY_OVERRIDES += \
+    zygote.critical_window.minute=10
