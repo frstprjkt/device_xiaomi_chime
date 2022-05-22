e9eae2c9ed5c597ebd2b6f1fc493a08d2a1c1b0d
Jabiyeff <cebiyevanar@gmail.com>
2022-05-22 13:50:35 +0400

    chime: usb: Initial adaptation for chime
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: RayapKun <rayapkun@gmail.com>
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: I9f181b208ef66828a8ee1d8c82334bf2848e096e
    Signed-off-by: Vayruz Rafli <vayruzrafli79@outlook.co.id>

--- device.mk
+++ device.mk
@@ -500,7 +500,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-chime \
+    android.hardware.usb.gadget@1.1-service-chime
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
